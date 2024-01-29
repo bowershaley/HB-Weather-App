5f2b61ab5c31c622cf27ce4fa84c117417a3217c
bowershaley <156343634+bowershaley@users.noreply.github.com>
2024-01-29 14:41:18 -0500

    add basic search engine no API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-<body>
-  <h1>SheCodes</h1>
-  <script src="app.js"></script>
-</body>;
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElemnt = document.querySelector("#search-form");
+searchFormElemnt.addEventListener("submit", handleSearchSubmit);
